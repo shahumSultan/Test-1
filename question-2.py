b64f278dc3b9890fc5b316d60048c1a7b8391f59
ShahumSultan <shahumsultan@hotmail.com>
2023-06-20 21:59:09 +0500

    second question

---
+++ question-2.py
@@ -0,0 +1,18 @@
+# Task (List Comprehension)
+# Given there are 4 integers a, y, z, n, perform the following actions:
+#   - x, y, z represent the dimensions of a cuboid, n is just an integer
+#   - Print a list of all possible coordinates given by (i, j, k) on a 3D grid where the sum of i+j+k is not equal to n.
+
+#Input Format => x, y, z, n are seperate integers, input on seperate lines
+#Constraints => Print the list in lexicographic increasing order.
+
+######code below this line######
+
+
+
+######do not change these lines, only add functionalites if needed######
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
