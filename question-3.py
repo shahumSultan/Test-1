f0431e7c1c7e55d2a81c223847f1520da31b7ab9
ShahumSultan <shahumsultan@hotmail.com>
2023-06-20 23:26:03 +0500

    question 3 added

---
+++ question-3.py
@@ -0,0 +1,16 @@
+# Task (Tuples)
+# Given there is n integer, perform the following actions:
+#   - Take the input for n, the number of elements to be added in the tuple
+#   - Add the elements in the tuple, no need to input the specific elements
+#   - Use the built-in function hash()
+#   - Print the tuple and the hash of the tuple
+
+#Input Format => n is the number of elements that will be in the tuple
+
+######code below this line######
+
+
+
+######do not change these lines, only add functionalites if needed######
+if __name__ == '__main__':
+    numElements = int(input("Enter number of elements: "))
