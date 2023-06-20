fbeb83262fde2602f35c94ebbd6e048076b74595
ShahumSultan <shahumsultan@hotmail.com>
2023-06-20 21:52:18 +0500

    first question commit

---
+++ question-1.py
@@ -0,0 +1,16 @@
+# Task (Loop with square of number)
+# Given there is an integer n, perform the following actions:
+#   - Take the value of n from the user 
+#   - Print the square of the number for n number of times
+#   - Create a function that prints the squre
+
+#Input Format => a positive integer 'n'
+#Constraints => 1 ­≤ n ≤ 20
+
+######code below this line######
+
+
+
+######do not change these lines, only add functionalites if needed######
+if __name__ == '__main__':
+    n = int(input("Enter Value of n: ").strip())
