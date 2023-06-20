261f92be00eb813b5eafb37bf0628a8bfe3162ab
ShahumSultan <shahumsultan@hotmail.com>
2023-06-20 23:53:55 +0500

    question 4 added

---
+++ question-4.py
@@ -0,0 +1,15 @@
+# Task (String Join and Split)
+# Given there is s string, perform the following actions:
+#   - Take the input for s, a string to join and split
+#   - Make a function that does both the join and the split
+#   - Print both the outputs at the same time
+
+#Input Format => s is string
+
+######code below this line######
+
+
+
+######do not change these lines, only add functionalites if needed######
+if __name__ == '__main__':
+    string = input("Enter a string: ")
