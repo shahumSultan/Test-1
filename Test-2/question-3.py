c5712ed39da1a971c3bc1482228cef13452c8dd8
Shahum <shahum@exarta.com>
2023-06-22 17:30:48 +0500

    added question 3

---
+++ Test-2/question-3.py
@@ -0,0 +1,11 @@
+# Task (Square-Cube-Lambda)
+# Perform the following actions:
+#   - Get integer from the user
+#   - Keep running the input loop until 'done' passed as input
+#   - Store these values in a list
+
+# Output:
+#   - A list with the squre of the numbers
+#   - A list with the cube of the numbers
+    
+# ----------- code below this line -----------
