c3e6c408aedfd0c183ff32047a7139d26922c308
Shahum <shahum@exarta.com>
2023-06-22 17:12:10 +0500

    question 1 added

---
+++ Test-2/question-1.py
@@ -0,0 +1,10 @@
+# Task (Dict-Lambda)
+# Perform the following actions:
+#   - Get two inputs from the user, first for name and second for age
+#   - Keep running the input the loop until 'done' passed as input
+#   - Store these values in a dict
+
+# Output:
+#   - Output the dict in sorted format
+    
+# ----------- code below this line -----------
