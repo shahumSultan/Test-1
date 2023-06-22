bcad9c439daeb1e458cfffa3a5607b0329bea35b
Shahum <shahum@exarta.com>
2023-06-22 17:22:04 +0500

    added question 2

--- Test-2/question-1.py
+++ Test-2/question-1.py
@@ -1,7 +1,7 @@
 # Task (Dict-Lambda)
 # Perform the following actions:
 #   - Get two inputs from the user, first for name and second for age
-#   - Keep running the input the loop until 'done' passed as input
+#   - Keep running the input loop until 'done' passed as input
 #   - Store these values in a dict
 
 # Output:
---
+++ Test-2/question-2.py
@@ -0,0 +1,11 @@
+# Task (Filter-Lambda)
+# Perform the following actions:
+#   - Get integer from the user
+#   - Keep running the input loop until 'done' passed as input
+#   - Store these values in a list
+
+# Output:
+#   - A list with only even numbers
+#   - A list with only odd numbers
+    
+# ----------- code below this line -----------
