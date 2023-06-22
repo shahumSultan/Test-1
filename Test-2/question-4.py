4373d5fbe8209e25c885d0b2dbc54b0f01b9ec5b
Shahum <shahum@exarta.com>
2023-06-22 17:38:08 +0500

    added question 4

---
+++ Test-2/question-4.py
@@ -0,0 +1,9 @@
+# Task (Sort-Lambda)
+# Perform the following actions:
+#   - Get integer from the user (can be -ive or +ive) in one line
+#   - Store these values in a list
+
+# Output:
+#   - A list with rearranged +ive and -ive numbers
+    
+# ----------- code below this line -----------
