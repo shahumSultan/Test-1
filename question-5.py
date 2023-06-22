6ce50143eb56ebcbf6ad228ef1cc1ad275f98862
Shahum <shahum@exarta.com>
2023-06-22 11:15:45 +0500

    last question

---
+++ question-5.py
@@ -0,0 +1,23 @@
+# Task (Calculate the angle)
+# Imagine a right angled triangle with sides AB, AC and BC
+# M is the midpoint of the hypotenuse AC, task it find the angle MBC in degress
+# Perform the following actions:
+#   - First input is the length of side AB 
+#   - Second input it the length of side BC
+#   - Calculate the angle MBC
+#   - Use {chr(176)} to print the degree sign
+
+# Input Format => AB and BC is float
+# Constrains
+#   - 0 < AB <= 100
+#   - 0 < BC <= 100
+
+import math
+
+######code below this line######
+def calculateAngle(len1, len2):
+    print(len1, len2)
+
+######do not change these lines, only add functionalites if needed######
+if __name__ == '__main__':
+    
